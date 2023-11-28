884f2533cdd2033986adb0a64544c07e834f6371
NourEddine0 <noureldine33@gmail.com>
2023-11-28 05:33:04 +0200

    Expression Evaluator Completed ,Remain NFA Handler

---
+++ ExpressionEvaluator.cpp
@@ -0,0 +1,177 @@
+#include "ExpressionEvaluator.h"
+
+ExpressionEvaluator :: ExpressionEvaluator(map<string, string> regularDefs): regularDefs(regularDefs) {}
+
+string ExpressionEvaluator :: removeRegExpressionDep (string expression) {
+        int start = 0;
+        string symbols = "()[]{}*+ |";
+        
+        while(start <= expression.length() - 1){
+            
+            if (symbols.find(expression[start]) == symbols.npos){
+                
+                string new_def = "";
+                int left = start;
+
+                while (start <= expression.length() - 1 && symbols.find(expression[start]) == symbols.npos)
+                    new_def += expression[start++];
+                
+                if (regularDefs.find(new_def) != regularDefs.end()) {
+                    expression.replace(left, start - left, "(" + regularDefs[new_def] + ")" );
+                    start = left;
+                }
+            }
+
+            start++;
+        }
+
+        return expression;
+}
+
+string ExpressionEvaluator :: infixToPostfix (string infix) {
+
+    vector<char> postfixStack; 
+    string postFix = "";
+    string operators = "()*+|^";
+    bool operand = false;
+
+    map<char, int> precedence = {
+        {'*', 3},
+        {'+', 3},
+        {'^', 2},
+        {'|', 1}
+    };
+
+    int i = 0;
+    string new_infix = "";
+    
+    while (i < infix.length()){
+    if (i != 0 && infix[i] == '(' && infix[i-1] != '|' && infix[i-1] != '(')
+        {
+            new_infix += "^";
+            new_infix += infix[i];
+        }
+    else if (i != infix.length()-1 && (infix[i] == ')' || infix[i] == '*' || infix[i] == '+') && operators.find(infix[i+1]) == operators.npos)
+        {
+            new_infix += infix[i];
+            new_infix += "^";
+        }
+    else
+        new_infix += infix[i];
+    i++;
+    }
+
+    infix = new_infix;
+
+
+    for(int i = 0; i < infix.length(); i++) {
+        if (operators.find(infix[i]) == operators.npos || i > 0 && operators.find(infix[i]) != operators.npos && infix[i-1] == '\\') {
+            postFix += infix[i];
+            operand = true;
+        }
+        else{
+            if (operand == true){
+                operand = false;
+                postFix += ' ';
+            }
+            if (infix[i] == '(') {
+                postfixStack.push_back('(');
+            }
+            else if (infix[i] == ')') {
+                while(postfixStack.back() != '(') {
+                    postFix += postfixStack.back();
+                    postfixStack.pop_back();
+                }
+                postfixStack.pop_back();
+            }
+            else {
+                while (!postfixStack.empty() && precedence[infix[i]] <= precedence[postfixStack.back()]) {
+                    postFix += postfixStack.back();
+                    postfixStack.pop_back();
+                }
+                postfixStack.push_back(infix[i]);
+            }
+        }
+
+    }
+
+    if (operand == true)
+        postFix += ' ';
+
+    while (!postfixStack.empty()) {
+        postFix += postfixStack.back();
+        postfixStack.pop_back();
+    }
+    return postFix;
+}
+
+string ExpressionEvaluator :: evaluatePostfix(string postfix){
+    
+    vector<string> stack;
+    string operators = "*+|^";
+    string operand = "";
+    int i = 0;
+    for (char c : postfix) {
+        if (operators.find(c) == operators.npos || operators.find(c) != operators.npos && i > 0 && postfix[i-1] == '\\' ) {
+            // Operand encountered, push onto the stack
+            if (c == ' '){
+                stack.push_back(operand);
+                operand = "";
+            } else {
+                if (operators.find(c) != operators.npos){
+                    operand = "";
+                    operand += c;
+                }
+                else operand += c; 
+            }
+
+        } else {
+            if (c == '*'){
+                string operand = stack.back();
+                stack.pop_back();
+                stack.push_back("kleen_closure(" + operand + ")");
+            }
+            else if (c == '+'){
+                string operand = stack.back();
+                stack.pop_back();
+                stack.push_back("kleen_closure+(" + operand + ")");
+            }
+            else if (c == '^'){
+                string operand2 = stack.back();
+                stack.pop_back();
+
+                string operand1 = stack.back();
+                stack.pop_back();
+
+                stack.push_back("concate(" + operand1 + ", " + operand2 + ")");
+            }
+            else{
+                string operand2 = stack.back();
+                stack.pop_back();
+
+                string operand1 = stack.back();
+                stack.pop_back();
+
+                stack.push_back("union(" + operand1 + ", " + operand2 + ")");
+            }
+        }
+        i++;
+    }
+
+    return stack.back();
+}
+
+string ExpressionEvaluator :: evaluateRegExpression(string reg){
+    reg = removeRegExpressionDep(reg);
+    
+    reg.erase(remove_if(reg.begin(), reg.end(), ::isspace), reg.end());
+
+    string postfix = infixToPostfix(reg);
+
+    string result = evaluatePostfix(postfix);
+
+    return result;
+}
+
+
+
